ff404e0c6c5da5e62cb839266d48fc26eda9a188
ya <104529495+xoxexxx@users.noreply.github.com>
2024-12-23 08:16:12 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js", 
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"), 
+  },
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "dist"), 
+    open: true, 
+    port: 5173, 
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html", 
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
