cf953ff329b2a315987166aba71f9a4391f7184e
ya <104529495+xoxexxx@users.noreply.github.com>
2025-02-16 08:46:05 +0300

    Update syntaxis.js

--- syntaxis.js
+++ syntaxis.js
@@ -1,3 +1,10 @@
+npm init -y  # Инициализация нового проекта Node.js
+npm install --save-dev webpack webpack-cli  # Установка Webpack и Webpack CLI
+npm install --save-dev babel-loader @babel/core @babel/preset-env  # Установка Babel для транспиляции кода
+npm install --save-dev html-webpack-plugin  # Плагин для автоматической генерации HTML
+
+
+
 #selector element is arrow function
 const elem = (param) => document.querySelector(param);
 
