789bc2ec91c420193c5d22ddd945fea08d644d50
ya <104529495+xoxexxx@users.noreply.github.com>
2025-02-25 10:28:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,42 @@ npm install --save-dev webpack webpack-cli  # Установка Webpack и Webp
 npm install --save-dev babel-loader @babel/core @babel/preset-env  # Установка Babel для транспиляции кода
 npm install --save-dev html-webpack-plugin  # Плагин для автоматической генерации HTML
 
-
-
-#selector element is arrow function
+//# selector element is arrow function
 const elem = (param) => document.querySelector(param);
 
-#location change is history function
+//#location change is history function
 history.replaceState(object, "", param);
+
+//#promise json db
+function getJSON() {
+  const promise = new Promise((resolve, reject) => {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+
+    xhr.onload = function() {
+      if (this.status == 200) {
+        resolve(this.response);
+      } else {
+        var error = new Error(this.statusText);
+        error.code = this.status;
+        reject(error);
+      }
+    };
+    xhr.onerror = function() {
+      reject(new Error("Network Error"));
+    };
+    xhr.send();
+  });
+}
+
+getJSON("./first-db.json").then(db => {
+    const data = JSON.parse(db);
+    return data
+  }).then((data) => getJSON("db-${data.param}.json")).then(
+    JSON.parse,
+    error => throw error
+  );
+
+
 
 
