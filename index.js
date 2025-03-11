d6235eca48cbd9672533f7f3119a838a4e6e86bd
ya <104529495+xoxexxx@users.noreply.github.com>
2025-03-12 01:33:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-npm init -y  # Инициализация нового проекта Node.js
-npm install --save-dev webpack webpack-cli  # Установка Webpack и Webpack CLI
-npm install --save-dev babel-loader @babel/core @babel/preset-env  # Установка Babel для транспиляции кода
-npm install --save-dev html-webpack-plugin  # Плагин для автоматической генерации HTML
+npm init -y  #Инициализация нового проекта Node.js
+npm install --save-dev webpack webpack-cli webpack-dev-server  #Установка Webpack и Webpack CLI
+npm install --save-dev concurrently #Запуск client-server
+npm install --svae-dev nodemon #Отслеживание изменений
+npm install --save-dev babel-loader @babel/core @babel/preset-env  #Установка Babel для транспиляции кода
+npm install --save-dev html-webpack-plugin  #Плагин для автоматической генерации HTML
+
+npm install express #server
 
 //# selector element is arrow function
 const elem = (param) => document.querySelector(param);
