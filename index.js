6ee47249719980a988e7c4bf3d8c539166a8353f
ya <104529495+xoxexxx@users.noreply.github.com>
2025-03-12 03:12:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-npm init -y  #Инициализация нового проекта Node.js
-npm install --save-dev webpack webpack-cli webpack-dev-server  #Установка Webpack и Webpack CLI
-npm install --save-dev concurrently #Запуск client-server
-npm install --svae-dev nodemon #Отслеживание изменений
-npm install --save-dev babel-loader @babel/core @babel/preset-env  #Установка Babel для транспиляции кода
-npm install --save-dev html-webpack-plugin  #Плагин для автоматической генерации HTML
+npm init -y  #init
+npm install --save-dev webpack webpack-cli webpack-dev-server  #install Webpack and Webpack CLI
+npm install --save-dev concurrently #create client-server render
+npm install --svae-dev nodemon #
+npm install --save-dev babel-loader @babel/core @babel/preset-env  #install Babel for code translate
+npm install --save-dev html-webpack-plugin  #plugin for html render
 
 npm install express #server
 
@@ -12,37 +12,4 @@ const elem = (param) => document.querySelector(param);
 
 //#location change is history function
 history.replaceState(object, "", param);
-
-//#promise json db
-function getREST() {
-  const promise = new Promise((resolve, reject) => {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-
-    xhr.onload = function() {
-      if (this.status == 200) {
-        resolve(this.response);
-      } else {
-        var error = new Error(this.statusText);
-        error.code = this.status;
-        reject(error);
-      }
-    };
-    xhr.onerror = function() {
-      reject(new Error("Network Error"));
-    };
-    xhr.send();
-  });
-}
-
-getREST("./first-db.json").then(db => {
-    const data = JSON.parse(db);
-    return data
-  }).then((data) => getREST("db-${data.param}.json")).then(
-    JSON.parse,
-    error => throw error
-  );
-
-
-
 
